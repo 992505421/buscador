d00205c42756a2eee533566dd0b50c8ebc312ff1
Crisley Matheus <Crisley1002@gmail.com>
2024-02-09 04:36:57 -0300

    estilo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
-import { IMaskInput  } from "react-imask";
+import { IMaskInput } from "react-imask";
 import { FaSearch } from "react-icons/fa";
-import Logo from './img/fundo.png'; 
+
 import './style.css';
 
 function App() {
   return (
-    <div className="container">
-      <img src={Logo} alt="Logo"/> 
-      <h1 className="title">Buscar CEP</h1>
-      <div className="containerInput">
-        <IMaskInput mask="00000-000" placeholder="Digite o seu CEP..." />
 
-        <button className="buttonSearch"><FaSearch className="buttonPesquisar" size={14} color="#fff"/>Procurar</button>
-      </div>
+    <div className="container">
+      <form className="container-form">
+        <h1 className="title">Buscar CEP</h1>
+        <div className="containerInput">
+          <IMaskInput mask="00000-000" placeholder="Digite o seu CEP..." />
+
+          <button className="buttonSearch">
+            <FaSearch className="buttonPesquisar" size={14} color="#fff" />
+            <span style={{ marginLeft: '2mm' }}>Procurar</span>
+          </button>
+        </div>
+      </form>
       <main className="main">
-        <h2>CEP: 76160000</h2> 
+        <h2>CEP: 76160000</h2>
         <span>Rua teste algum</span>
         <span>Complemento:</span>
         <samp>Vila</samp>
